39506d7a138b14b2d45bdfbf85d236486ae730b9
sumaiyaava <sumaiyarahamanava11@gmail.com>
2025-10-25 20:16:27 +0600

    update

--- phase 3/day 1/script.js
+++ phase 3/day 1/script.js
@@ -1,3 +1,16 @@
 //var,let,const
 var name1 = "Ava"; 
 let name2 = "CPC";
+const name3 = "OpenAI";
+console.log(name1); 
+console.log(name2); 
+console.log(name3);        
+name1 = "Liam";
+name2 = "ChatGPT";  
+
+console.log(name1); 
+console.log(name2);      
+console.log(name3); 
+
+
+
