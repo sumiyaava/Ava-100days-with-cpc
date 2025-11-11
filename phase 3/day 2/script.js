f001fa8ebf207ca065e5c48ec62da22d2feb0d32
sumaiyaava <sumaiyarahamanava11@gmail.com>
2025-11-11 20:01:19 +0600

    update

---
+++ phase 3/day 2/script.js
@@ -0,0 +1,21 @@
+ <script>
+    
+    var name = "Sumaiya";
+    console.log("Using var:", name);
+
+    
+    let age = 22;
+    console.log("Using let:", age);
+
+
+    const country = "Bangladesh";
+    console.log("Using const:", country);
+
+    
+    name = "Ava"; 
+    age = 23;     
+    
+
+    console.log("Updated name:", name);
+    console.log("Updated age:", age);
+  </script>
