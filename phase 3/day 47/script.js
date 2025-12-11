7491741b7e4eafc2501026618d6804370b8a7d4a
Sumiya Rahaman Ava <sumaiyarahamanava11@gmail.com>
2025-12-11 07:42:29 -0800

    Add files via upload

---
+++ phase 3/day 47/script.js
@@ -0,0 +1,38 @@
+function runJSOperations() {
+    let a = 10;
+    let b = 3;
+
+    console.log("Addition:", a + b);
+    console.log("Subtraction:", a - b);
+    console.log("Multiplication:", a * b);
+    console.log("Division:", a / b);
+    console.log("Modulus:", a % b);
+
+    console.log("Is a > b?", a > b);
+    console.log("Is a == b?", a == b);
+    console.log("Is a !== b?", a !== b);
+
+    let x = true;
+    let y = false;
+
+    console.log("AND:", x && y);
+    console.log("OR:", x || y);
+    console.log("NOT x:", !x);
+
+    let num = 5;
+    num += 2;
+    console.log("num after += 2:", num);
+
+    let first = "Hello";
+    let last = "World";
+    console.log(first + " " + last);
+
+    console.log(" JS operations finished successfully!");
+
+    
+    document.getElementById("msg").innerText = 
+        "JS operations completed! Check the console for details.";
+    document.getElementById("msg").style.color = "green";
+}
+
+document.getElementById("runBtn").addEventListener("click", runJSOperations);
